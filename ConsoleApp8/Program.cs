fe55acd25c3ef49c5af406777018a0abc9bc2a5e
unknown <alifali@code.edu.az>
2022-02-09 22:27:26 +0400

    task1_added

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    internal class Homework
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("students");
+
+            string[] students = { "Anar", "Pasa", "Vasif", "Iqbal", "Elvin" };
+
+            foreach (string student in students) ;
+            {
+
+            }
+            if (letter == 'c') ; 
+
+            Console.WriteLine(students);
+            
+
+          
+           
+
+                 }  
+       }
+}
