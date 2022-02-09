c3f0656c619ec14dca52199508d09b023b4fce1a
unknown <alifali@code.edu.az>
2022-02-10 02:04:08 +0400

    task1_added

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+        int num = Convert.ToInt32(Console.ReadLine());
+        int count = 1;
+
+        if(num <= 50);
+        {
+
+                for (int i = 4; i <=num ; i++);
+                {
+                    if(i%3 == 0);
+                   {
+                       if (count++);
+            
+                    }
+                }
+            }  else if(num > 50, && num <= 100) ;
+
+            {
+    for (int i = 6; i <= num; i++) ;
+    {
+        if (i % 5 == 0) ;
+
+
+    }  count++; 
+                
+                
+            
+            else
+{
+    for (int i = 9; i <= num; i++) ;
+        {
+            if (i % 8 == 0) ;
+        }
+        
+             count++;
+        }
+            
+    }
+    
+
+
+
+
+
+
+
+
+
